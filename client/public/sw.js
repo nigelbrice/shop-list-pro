43fadca364565ad661dc60d266f754d35d96384e
nigelbrice <brice.nigel@gmail.com>
2026-03-08 17:29:26 +0000

    added service handler

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,7 +1,23 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
+const CACHE = "shopeeze-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
